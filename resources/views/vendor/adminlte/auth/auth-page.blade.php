ecd4b52caf1a7f1dbc2ad5ed8ea740c878698089
Ezequiel Fischer <ezequielfischer1999@gmail.com>
2024-12-25 18:12:38 -0400

    07 Como instalar Laravel - Permission en el Sistema de Ventas

--- resources/views/vendor/adminlte/auth/auth-page.blade.php
+++ resources/views/vendor/adminlte/auth/auth-page.blade.php
@@ -56,7 +56,7 @@
         </div>
 
         {{-- Card Box --}}
-        <div class="card {{ config('adminlte.classes_auth_card', 'card-outline card-primary') }}">
+        <div class="card {{ config('adminlte.classes_auth_card', 'card-outline card-primary') }}" style="box-shadow: 5px 5px 5px 5px #cccccc">
 
             {{-- Card Header --}}
             @hasSection('auth_header')
